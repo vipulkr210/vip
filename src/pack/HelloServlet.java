0f630290a4fb5eb95b5b5b2f6dbd98e571fcf5bd
VIPUL KUMAR <vipulkr210@gmail.com>
2017-12-31 15:49:59 +0530

    Add files via upload

---
+++ src/pack/HelloServlet.java
@@ -0,0 +1,42 @@
+package pack;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet("/HelloServlet")
+public class HelloServlet extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String uname=request.getParameter("uname");
+		String name=request.getParameter("name");
+		
+		response.setContentType("text/pdf");
+		PrintWriter out=response.getWriter();
+		out.println("Uname is: "+uname);
+		out.println("Name is: "+name);
+		
+		
+		
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
